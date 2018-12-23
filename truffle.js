69b64554d0f2ab473edbe62a937a1680da760268
Duong Khong <duong1521991@gmail.com>
2018-12-23 20:23:56 +0700

    Bug fixed

--- truffle.js
+++ truffle.js
@@ -12,13 +12,13 @@
  *   },
  */
 
-const constants = require('./constants/constants')
-const KOVAN_RPC = constants.KOVAN_RPC;
-const KOVAN_NETWORK_ID = constants.KOVAN_NETWORK_ID;
-const KOVAN_TX_DEFAULTS = constants.KOVAN_TX_DEFAULTS;
-const ROPSTEN_RPC = constants.ROPSTEN_RPC;
-const ROPSTEN_NETWORK_ID = constants.ROPSTEN_NETWORK_ID;
-const DEV_PRIVATE_KEY = constants.DEV_PRIVATE_KEY;
+// const constants = require('./constants/constants')
+// const KOVAN_RPC = constants.KOVAN_RPC;
+// const KOVAN_NETWORK_ID = constants.KOVAN_NETWORK_ID;
+// const KOVAN_TX_DEFAULTS = constants.KOVAN_TX_DEFAULTS;
+// const ROPSTEN_RPC = constants.ROPSTEN_RPC;
+// const ROPSTEN_NETWORK_ID = constants.ROPSTEN_NETWORK_ID;
+// const DEV_PRIVATE_KEY = constants.DEV_PRIVATE_KEY;
 const ProviderEngine = require('web3-provider-engine');
 const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
 const FetchSubprovider = require("web3-provider-engine/subproviders/fetch.js");
@@ -34,35 +34,35 @@ module.exports = {
             network_id: "*" // match any network
         },
         
-        kovan: {
-            from: constants.DEV_ADDRESS,
-            network_id: KOVAN_NETWORK_ID,      
-            gasPrice: 200000000000,
-            provider: function () {
-                const engine = new ProviderEngine();        
-                const privateKeyWalletSubprovider = new PrivateKeyWalletSubprovider(DEV_PRIVATE_KEY);
-                const filterSubprovider = new FilterSubprovider();
-                engine.addProvider(privateKeyWalletSubprovider);        
-                engine.addProvider(filterSubprovider);
-                engine.addProvider(new FetchSubprovider({ rpcUrl: KOVAN_RPC }));
-                engine.start();
-                return engine;
-            },
-        },
+        // kovan: {
+        //     from: constants.DEV_ADDRESS,
+        //     network_id: KOVAN_NETWORK_ID,      
+        //     gasPrice: 200000000000,
+        //     provider: function () {
+        //         const engine = new ProviderEngine();        
+        //         const privateKeyWalletSubprovider = new PrivateKeyWalletSubprovider(DEV_PRIVATE_KEY);
+        //         const filterSubprovider = new FilterSubprovider();
+        //         engine.addProvider(privateKeyWalletSubprovider);        
+        //         engine.addProvider(filterSubprovider);
+        //         engine.addProvider(new FetchSubprovider({ rpcUrl: KOVAN_RPC }));
+        //         engine.start();
+        //         return engine;
+        //     },
+        // },
 
-        ropsten: {
-            from: constants.DEV_ADDRESS,
-            network_id: ROPSTEN_NETWORK_ID,
-            provider: function () {
-                const engine = new ProviderEngine();        
-                const privateKeyWalletSubprovider = new PrivateKeyWalletSubprovider(DEV_PRIVATE_KEY);
-                const filterSubprovider = new FilterSubprovider();
-                engine.addProvider(privateKeyWalletSubprovider);        
-                engine.addProvider(filterSubprovider);
-                engine.addProvider(new FetchSubprovider({ rpcUrl: ROPSTEN_RPC }));
-                engine.start();
-                return engine;
-            },
-        },
+        // ropsten: {
+        //     from: constants.DEV_ADDRESS,
+        //     network_id: ROPSTEN_NETWORK_ID,
+        //     provider: function () {
+        //         const engine = new ProviderEngine();        
+        //         const privateKeyWalletSubprovider = new PrivateKeyWalletSubprovider(DEV_PRIVATE_KEY);
+        //         const filterSubprovider = new FilterSubprovider();
+        //         engine.addProvider(privateKeyWalletSubprovider);        
+        //         engine.addProvider(filterSubprovider);
+        //         engine.addProvider(new FetchSubprovider({ rpcUrl: ROPSTEN_RPC }));
+        //         engine.start();
+        //         return engine;
+        //     },
+        // },
     }
 };
