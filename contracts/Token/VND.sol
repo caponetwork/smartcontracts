00bc52bbccde216e5628e54ad8ca5c8e74a92bfb
Duong Khong <duong1521991@gmail.com>
2019-02-27 11:09:03 +0700

    Create vnd token smartcontract

---
+++ contracts/Token/VND.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+
+contract VND is ERC20, ERC20Detailed, ERC20Burnable, ERC20Mintable {
+
+  constructor() public ERC20Detailed("Vietnam Dong", "VND", 18) {}
+}
